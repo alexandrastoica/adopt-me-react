3297d32e618295ae487dab86ab1a9bb20be01cb0
Alexandra Stoica <ralexandrastoica@gmail.com>
2021-01-18 15:32:06 +0200

    Delete test components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import React from "react";
 import { render } from "react-dom";
-import { Router } from '@reach/router';
-
-import Footer from "./components/Footer";
-import Main from "./components/Main";
-import Article from "./pages/Article";
-
+import { Router } from "@reach/router";
 
 const App = () => {
   return (
     <React.StrictMode>
-      <Router>
-        <Main path="/" />
-        <Article path="/articles/:title" />
-      </Router>
-      <Footer />
+      <Router></Router>
     </React.StrictMode>
   );
 };
