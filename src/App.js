2f0176887fdb2ac2db6181d2c7fb3c2dda6cc994
Alexandra Stoica <ralexandrastoica@gmail.com>
2021-01-19 17:10:20 +0200

    Add a navbar component styled with emotion

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import { render } from "react-dom";
-import { Router, Link } from "@reach/router";
+import { Router } from "@reach/router";
 import Details from "./pages/Details";
 import SearchParams from "./components/SearchParams";
 import ThemeContext from "./ThemeContext";
+import NavBar from "./components/NavBar";
 
 const App = () => {
   const themeHook = useState("rebeccapurple");
@@ -11,9 +12,7 @@ const App = () => {
   return (
     <React.StrictMode>
       <ThemeContext.Provider value={themeHook}>
-        <header>
-          <Link to="/">Adopt me!</Link>
-        </header>
+        <NavBar />
         <Router>
           <SearchParams path="/" />
           <Details path="/details/:id" />
