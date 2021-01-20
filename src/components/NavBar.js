13c8046f0bd655010d0a5fd09ac9f1cbf36bc484
Alexandra Stoica <ralexandrastoica@gmail.com>
2021-01-20 10:00:08 +0200

    Add some styles

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,35 @@
 import React from "react";
 import { Link } from "@reach/router";
-import { css } from "@emotion/react";
+import { css, keyframes } from "@emotion/react";
+import colors from "../styles/colors";
+
+const spin = keyframes`
+  to {
+    transform: rotate(360deg);
+  }
+`;
 
 const NavBar = () => (
   <header
     css={css`
-      background-color: #333;
+      background-color: ${colors.secondary};
       padding: 15px;
     `}
   >
     <Link to="/">Adopt me!</Link>
-    <span role="img" aria-label="logo">
+    <span
+      css={css`
+        cursor: default;
+        font-size: 60px;
+        display: inline-block;
+
+        &:hover {
+          animation: 2s ${spin} linear infinite;
+        }
+      `}
+      role="img"
+      aria-label="logo"
+    >
       🐶
     </span>
   </header>
